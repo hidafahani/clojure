8d172e798af662208025b48a19fc5581b689b275
Syahidah Isfahani <55047407+hidafahani@users.noreply.github.com>
2022-05-12 22:50:47 +0700

    update

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.slf4j/slf4j-simple "1.7.28"]
                  [io.pedestal/pedestal.service "0.5.8"]
                  [io.pedestal/pedestal.jetty "0.5.8"]
                  [io.pedestal/pedestal.route "0.5.8"]
+                 [org.slf4j/slf4j-simple "1.7.28"]
                  [hiccup/hiccup "2.0.0-alpha2"]
                  [cljs-ajax "0.2.6"]
                  [reagent "1.0.0"]
